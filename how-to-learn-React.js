659bd5765717f068f627d72a154465e52ddf27a1
ynit2000 <84170843+ynit2000@users.noreply.github.com>
2024-03-09 16:38:00 +0530

    how-to-learn-React

---
+++ how-to-learn-React.js
@@ -0,0 +1,58 @@
+
+// React Roadmap :
+
+// why to learn React ? 
+// -> hype , job, trend, build UI 
+// -> make easy to manage & build complex frontend
+
+// when should I learn React ?
+// -> After mastering JS 
+// -> most project don't need react in initial phase.
+
+// why react was created 
+// -> Ghost message Problem :
+
+//   state --> JS and UI -> DOM 
+//    khan Academy --< Unsplash
+
+// Don't learn React if :
+// -> you don't know how JS work OR Dom works 
+
+
+// React Learning Process :
+// 1. Go in depth               2. by making Projects (one topic at a time)
+//      |                                   | 
+//    BroadcastChannel,fibre,         todo,calculator,github APi
+//    Virtual DOM,diff algo, 
+//    hydration
+
+
+// React is Library
+//  -> FrameWork  Vs Library 
+//    (military)      (cool dude)
+
+
+// Topic To Learn : 
+// -> core of React ( State or UI manipulation, JSX)
+// -> Component Reusability
+// -> Reusing of Componet (Props)
+// -> How to Propagate change (hooks)
+
+// Additional Add on To React :
+// -> Rortex (React don't have Router)
+// -> State management (React don't have state managemnet )
+//      -> Redax , Redux bolkit, zustand, context APi
+// -> Class based Componetn 
+//      -> Legacy code 
+
+// -> BAAS Apps :
+//    -> social media Clone , e-commerce App..App 
+
+    
+// After React :
+// -> React is not a complete solution in most case :
+//    -> no sessionStorage, browser Render of JS , no Reuting 
+// -> FrameWork:
+//   -> Next JS , Gotsby, Remix
+    
+
