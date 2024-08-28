06f7367c7907aae8d70718d22dc803c8f02db373
ynit2000 <84170843+ynit2000@users.noreply.github.com>
2024-08-28 12:49:21 +0530

    BasicReact and Basic Vite

---
+++ 01basicvite/src/YouFunction.jsx
@@ -0,0 +1,12 @@
+
+function YouFunction() {
+
+    return (
+      <>
+        <h1>Hello! This Vite App by YouFunction</h1>
+        <h1>Hello! This Vite App</h1>
+      </>
+    )
+  }
+  
+  export default YouFunction
